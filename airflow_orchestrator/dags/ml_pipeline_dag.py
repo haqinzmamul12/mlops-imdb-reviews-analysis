9aecd25ebc1e7a72c2ee4561bad0370dfa36370b
haqinzmamul12 <haqinzmamul12@gmail.com>
2025-08-20 12:19:47 +0530

    run airflow pipelines

--- airflow_orchestrator/dags/ml_pipeline_dag.py
+++ airflow_orchestrator/dags/ml_pipeline_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 from src.data.load_data import load_imdb_data
 from src.features.text_cleaning import DataTransformation
